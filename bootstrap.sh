aebf5e01be923b6427e9bb2438ea3e92cac1f195
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2025-10-25 13:09:28 -0400

    install firefox from mozilla debian repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -63,8 +63,26 @@ then
     # Add the Mullvad repository server to apt
     echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable stable main" | sudo tee /etc/apt/sources.list.d/mullvad.list
     echo "Mullvad repo added"
-
+    echo "Adding repo for Firefox"
+    # from: https://support.mozilla.org/en-US/kb/install-firefox-linux#w_install-firefox-deb-package-for-debian-based-distributions-recommended
+    # 1. Create a directory to store APT repository keys if it doesn't exist: 
+    sudo install -d -m 0755 /etc/apt/keyrings 
+    # 2. Import the Mozilla APT repository signing key: 
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+    # 3. The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command: 
+    gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+    # 4. Next, add the Mozilla APT repository to your sources.list: 
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+    # 5. Configure APT to prioritize packages from the Mozilla repository: 
+    echo '
+    Package: *
+    Pin: origin packages.mozilla.org
+    Pin-Priority: 1000
+    ' | sudo tee /etc/apt/preferences.d/mozilla 
+    # 6. Update your package list, and install firefox
+    sudo apt-get remove -y firefox
     sudo apt-get update
+    sudo apt-get install -y firefox
     echo "Updating firmware for available devices"
     sudo fwupdmgr get-devices 
     sudo fwupdmgr refresh --force 
