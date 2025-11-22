4be52892b816447660b911381415b8d34a96dace
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2025-11-22 15:42:53 -0500

    fix potential bugs with ssh key gen and setup, add .npmrc config to silence npm updates

--- gen_ssh_key.sh
+++ gen_ssh_key.sh
@@ -1,7 +1,12 @@
-mkdir ~/.ssh
+#!/bin/bash
+# generate ssh key for github usage
+mkdir -p ~/.ssh
 #some docs for below are from: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+echo "Creating SSH key..."
 ssh-keygen -t ed25519 -C "faizan.jamilwork@gmail.com"
-
+mv ~/.ssh/id_ed25519 ~/.ssh/github_id_ed25519
+mv ~/.ssh/id_ed25519.pub ~/.ssh/github_id_ed25519.pub
+echo "SSH key created:"
 # add to ssh agent
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github_id_ed25519
--- mac-bootstrap.sh
+++ mac-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # REQUIRES THE FOLLOWING FILES TO BE IN THE SAME DIRECTORY
 # .zshrc
-# github-keygen
+# gen_ssh_key
 # Brewfile
 
 # modified from https://github.com/newsdaycom/dev/blob/master/osx_setup/dev_setup
@@ -20,32 +20,41 @@ fi
 clear
 #source ~/.zshrc
 echo "Installing brew packages"
-/opt/homebrew/bin/brew bundle install --force
+cd /opt/homebrew/bin/
+brew bundle install --force
 
 
-/opt/homebrew/bin/brew doctor
+brew doctor
 
 echo "Installing Oh My ZSH"
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-#rm ~/.zshrc
 
-#echo "COPY OVER .zshrc FILE FROM OTHER COMPUTER"
-
-#read -n 1 -p "Press any key once that's complete"
 # mv ~/.zshrc-newsday ~/.zshrc
-
+echo "Installing nvm"
+# install nvm without touching .zshrc
+PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash'
+echo "nvm installed"
 echo "Installing deno"
 curl -fsSL https://deno.land/install.sh | bash -s -- --yes --no-modify-path
 echo "Deno installed"
-source ~/.zshrc
-echo "Installing latest node.js LTS version"
-fnm install --lts
-echo "Node.js is installed"
 
+rm ~/.zshrc
+echo "COPY OVER .zshrc FILE FROM OTHER COMPUTER"
+read -n 1 -p "Press any key once that's complete"
+source ~/.zshrc
+
+echo "Installing latest node.js LTS version"
+cd ~/.nvm
+nvm install --lts
+echo "Node.js LTS is installed"
+
+echo "copying .npmrc to silence npm updates"
+cp .npmrc ~/.npmrc
+echo "npm configured"
 echo "Installation complete!"
 echo "Enabling show hidden files in Finder"
 defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder;
 echo "Hidden files now shown in Finder"
 echo "Generating SSH key, add this to github account then run repo setup script"
-bash github-keygen
+bash gen_ssh_key
 echo "Please reboot your system and add your SSH key to Github before running the newsday-setup-repos script"
