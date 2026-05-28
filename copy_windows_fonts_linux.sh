cedc8454daecce9365f5f682a8a73bd06100b81c
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-05-28 13:45:33 -0400

    add sudo to font commands assuming running as non-root and add instructions for creating user

--- copy_windows_fonts_linux.sh
+++ copy_windows_fonts_linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # copy Windows fonts to Linux
-mkdir -p /usr/local/share/fonts/WindowsFonts
+sudo mkdir -p /usr/local/share/fonts/WindowsFonts
 # unzip fonts to folder
-unzip ms_fonts.zip -d /usr/local/share/fonts/WindowsFonts
+sudo unzip ms_fonts.zip -d /usr/local/share/fonts/WindowsFonts
 # set perms
-chmod -R 644 /usr/local/share/fonts/WindowsFonts/*
+sudo chmod -R 644 /usr/local/share/fonts/WindowsFonts/*
 # reset font cache
-fc-cache --force
-fc-cache-32 --force
+sudo fc-cache --force
+sudo fc-cache-32 --force
