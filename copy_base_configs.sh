96739cc67ea773b09fff745a7042b7aacf9fd6c4
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-06-01 10:51:31 -0400

    copy reflector config to new system

--- copy_base_configs.sh
+++ copy_base_configs.sh
@@ -1,6 +1,7 @@
 echo "Copying base configuration files (lightdm, journald, systemd-boot and systemd-boot hook) to their respective locations..."
 sudo cp .config/journald.conf /etc/systemd/journald.conf
 sudo cp .config/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp .config/reflector.conf /etc/xdg/reflector/reflector.conf
 # systemd-boot files are located in esp, which is usually mounted at /boot, so we are copying the files there
 sudo cp .config/systemd-boot/loader.conf /boot/loader/loader.conf
 sudo cp .config/systemd-boot/entries/arch.conf /boot/loader/entries/arch.conf 
