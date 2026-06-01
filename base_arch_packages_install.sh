811b1aee318411be97f04762cf3273bf68a30b3e
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-06-01 10:52:19 -0400

    add notice to base arch package script

--- base_arch_packages_install.sh
+++ base_arch_packages_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+# RUN FROM ARCH INSTALLATION MEDIA, NOT FROM THE INSTALLED SYSTEM
 # install by downloading this script and running it with bash
+
 # curl -s -L "https://raw.githubusercontent.com/faizjamil/dot-files-public/main/base_arch_packages_install.sh" | bash
 echo "Backing up existing mirrorlist to /etc/pacman.d/mirrorlist.backup"
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
