5f0b860a15d04269435eb9c24eac901b5ca75b4c
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-06-01 21:22:17 -0400

    install and copy config for chrony

--- copy_base_configs.sh
+++ copy_base_configs.sh
@@ -1,7 +1,10 @@
-echo "Copying base configuration files (lightdm, journald, systemd-boot and systemd-boot hook) to their respective locations..."
+echo "Copying base configuration files (lightdm, journald, systemd-boot, chrony and systemd-boot hook) to their respective locations..."
 sudo cp .config/journald.conf /etc/systemd/journald.conf
 sudo cp .config/lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp .config/reflector.conf /etc/xdg/reflector/reflector.conf
+# default chrony location from: https://man.archlinux.org/man/chrony.conf.5#DESCRIPTION
+# chrony config might also be in /etc/chrony/chrony.conf
+sudo cp .config/chrony.conf /etc/chrony.conf
 # systemd-boot files are located in esp, which is usually mounted at /boot, so we are copying the files there
 sudo cp .config/systemd-boot/loader.conf /boot/loader/loader.conf
 sudo cp .config/systemd-boot/entries/arch.conf /boot/loader/entries/arch.conf 
