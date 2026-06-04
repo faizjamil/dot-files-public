2419b012e0f9cf5d0fcfb0b0549c5f49ea882485
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-06-04 14:59:19 -0400

    create directories to copy configs to

--- copy_base_configs.sh
+++ copy_base_configs.sh
@@ -1,14 +1,18 @@
 echo "Copying base configuration files (lightdm, journald, systemd-boot, chrony and systemd-boot hook) to their respective locations..."
+sudo mkdir -p /etc/systemd
 sudo cp .config/journald.conf /etc/systemd/journald.conf
 # sudo cp .config/lightdm.conf /etc/lightdm/lightdm.conf
+sudo mkdir -p /etc/xdg/reflector
 sudo cp .config/reflector.conf /etc/xdg/reflector/reflector.conf
 # default chrony location from: https://man.archlinux.org/man/chrony.conf.5#DESCRIPTION
 # chrony config might also be in /etc/chrony/chrony.conf
 sudo cp .config/chrony.conf /etc/chrony.conf
 # systemd-boot files are located in esp, which is usually mounted at /boot, so we are copying the files there
+sudo mkdir -p /boot/loader/entries
 sudo cp .config/systemd-boot/loader.conf /boot/loader/loader.conf
 sudo cp .config/systemd-boot/entries/arch.conf /boot/loader/entries/arch.conf 
-sudo cp .config/systemd-boot/entries/arch-fallback.conf /boot/loader/entries/arch-fallback.conf
+# sudo cp .config/systemd-boot/entries/arch-fallback.conf /boot/loader/entries/arch-fallback.conf
+sudo mkdir -p /etc/pacman.d/hooks
 sudo cp .config/systemd-boot/95-systemd-boot.hook /etc/pacman.d/hooks/95-systemd-boot.hook
 echo "Base configuration files copied successfully."
 sudo systemctl enable NetworkManager.service
